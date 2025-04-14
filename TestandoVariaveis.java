6e0c8f460962d7b6c880ff98a2ad5dc806fbb57b
SamuelSouza85 <samueldesouza859@gmail.com>
2025-04-14 19:56:10 -0300

    Added TestantoVariavel.java and TestandoVarialvel.class

---
+++ TestandoVariaveis.java
@@ -0,0 +1,9 @@
+public class TestandoVariaveis {
+    public static void main(String[] args) {
+        char masculino = 'A';
+        char feminino = 'A';
+        String nome = "Aqui você pode colocar uma frase bem grande";
+        boolean condicao = true;
+        System.out.println(condicao);
+    }
+}
