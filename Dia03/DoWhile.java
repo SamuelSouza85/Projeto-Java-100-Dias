2f77f65eb2d2ee40fb2782985b7ee23a4df1aed6
SamuelSouza85 <samueldesouza859@gmail.com>
2025-05-13 21:25:12 -0300

    Corriginda uma palavra no arquivo DoWhile

--- Dia03/DoWhile.java
+++ Dia03/DoWhile.java
@@ -2,10 +2,10 @@ public class DoWhile {
     public static void main(String[] args) {
         boolean condicao = false;
         while (condicao){
-            System.out.println("dentrp do while");
+            System.out.println("dentro do while");
         }
         do {
-            System.out.println("dentrp do do-while");
+            System.out.println("dentro do do-while");
         }while(condicao);
     }
 }
