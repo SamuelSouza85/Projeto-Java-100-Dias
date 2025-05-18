16207266c7ff0e930c6d28104267430d29e76b25
SamuelSouza85 <samueldesouza859@gmail.com>
2025-05-17 22:10:01 -0300

    Adicionando pacote Dia007

---
+++ src/Dia007/Funcionario.java
@@ -0,0 +1,11 @@
+package Dia007;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public double []salario = new double[3];
+    public double media;
+
+    public void imprime() {
+    }
+}
