30c7f7dd8af7aec3a2cf40182a190d8c1b8805a3
SamuelSouza85 <samueldesouza859@gmail.com>
2025-05-18 21:23:57 -0300

    Adicionando pacote Dia008

---
+++ src/Dia008/Teste/AnimeTeste.java
@@ -0,0 +1,13 @@
+package Dia008.Teste;
+
+import Dia008.Anime;
+
+public class AnimeTeste {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        //anime.init("Akuma Drive", "TV", 12);
+        anime.init("Akuma Drive", "TV", 12, "Açao");
+        anime.imprime();
+
+    }
+}
