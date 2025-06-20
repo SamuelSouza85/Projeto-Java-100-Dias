76a1d5de379ddca5f974711ac603734d68c43c64
SamuelSouza85 <samueldesouza859@gmail.com>
2025-06-20 20:18:19 -0300

    Adicionando pacote Dia041

---
+++ src/Dia041/Colecoes/Teste/EqualsTeste01.java
@@ -0,0 +1,11 @@
+package Dia041.Colecoes.Teste;
+
+import Dia041.Colecoes.Dominio.Smartphone;
+
+public class EqualsTeste01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("1ABC1", "iPhone");
+        System.out.println(s1.equals(s2));
+    }
+}
